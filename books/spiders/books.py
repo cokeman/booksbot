5d509122627465e3141c31ca6768cfb50f7ed45c
cokeman <panksft@hotmail.com>
2020-12-22 17:39:42 +0000

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -4,13 +4,13 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "books"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["www.idealista.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://www.idealista.com/pro/century21-capital-las-palmas/',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+        for book_url in response.css("article.item-multimedia-container > a.item-link ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
         next_page = response.css("li.next > a ::attr(href)").extract_first()
         if next_page:
@@ -18,13 +18,11 @@ class BooksSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
         item = {}
-        product = response.css("div.product_main")
+        product = response.css("div.detail-container")
         item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
+        item['category'] = response.css('span.main-info__title-minor ::text').extract_first()
         item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
+            "//div[@class='details-property_features'] > ul/following-sibling::li/text()"
         ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        item['price'] = response.css('span.info-data-price ::text').extract_first()
         yield item
